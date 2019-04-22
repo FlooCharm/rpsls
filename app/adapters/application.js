aa3f7d43ccce06109c3e0f6698e2b84bcbaefa2e
Jessica Sánchez <michelle.sr16@gmail.com>
2019-04-22 08:25:36 -0500

    added adapter

---
+++ app/adapters/application.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
+
+export default class ApplicationAdapter extends ActiveModelAdapter {
+	host = 'https://polar-basin-61192.herokuapp.com/'
+}
