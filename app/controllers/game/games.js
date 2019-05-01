312ba50121c54d4ab253b09324f5d28ad133b086
Jessica Sánchez <michelle.sr16@gmail.com>
2019-05-01 17:04:09 -0500

    removed unused tracked property

--- app/controllers/game/games.js
+++ app/controllers/game/games.js
@@ -1,10 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
-export default class GameGamesController extends Controller {
-	@tracked userId;
-	
+export default class GameGamesController extends Controller {	
 	@action
 	createGame () {
 		alert('game created!');
