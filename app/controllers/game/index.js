e84037e4ef08c91282e69dfc83f9cec042918c1c
Jessica Sánchez <michelle.sr16@gmail.com>
2019-05-01 16:08:15 -0500

    added ember-power-select

--- app/controllers/game/index.js
+++ app/controllers/game/index.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class GameIndexController extends Controller {
+	@tracked selectedUser;
+
 	@action
 	enterGame () {
 		alert('entering game!');
