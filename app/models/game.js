12eb5c7ab74caa1fdf924c529ba7f070b3411e03
Jessica Sánchez <michelle.sr16@gmail.com>
2019-05-01 14:02:08 -0500

    added number of players on game-card component

--- app/models/game.js
+++ app/models/game.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
-const { Model, attr, /*belongsTo*/ } = DS;
+const { Model, attr, belongsTo } = DS;
 
 export default class GameModel extends Model {
 	@attr('string') code;
 	@attr('number') status;
+	@attr('number') totalUsers;
 
-	// @belongsTo('user') winner;
+	@belongsTo('user') winner;
 }
