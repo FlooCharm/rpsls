16896ac50ee39c8238d8ecfcdcb2d5a6824611c4
Roberto Huerta <beto4600@gmail.com>
2019-05-02 08:18:31 -0500

    feat: added polling in games/index for game model

--- app/controllers/game/detail.js
+++ app/controllers/game/detail.js
@@ -1,4 +1,18 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class GameDetailController extends Controller {
+	@action
+	selectMove (choice) {
+		this._poll = setInterval(this._pollFunction, 2000);
+	}
+
+	_pollFunction () {
+		console.log('cuac')
+	}
+
+	@action 
+	startGame () {
+		alert('game start!')
+	}
 }
--- app/routes/game/index.js
+++ app/routes/game/index.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import { action } from '@ember/object';
 
 export default class GameIndexRoute extends Route {
 	model () {
 		return this.store.findAll('game');
+	}
+
+	@action
+	didTransition () {
+		this._poll = setInterval(async () => {
+			this.controller.set('model', await this.store.findAll('game'));
+		}, 5000);
+	}
+
+	@action
+	willTransition () {
+		clearInterval(this._poll);
 	}
 }
